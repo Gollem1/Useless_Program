21349edc5604ebf1c488e02fc8a333030ac619ec
Chris <chrisgood404@gmail.com>
2017-07-12 16:15:01 -0400

    Update UselessProgram.py

--- UselessProgram.py
+++ UselessProgram.py
@@ -2,9 +2,9 @@ import win32api as wa
 import win32con as wcon
 import win32com.client as client
 
-wa.SetCursorPos((3000,1))
-wa.mouse_event(wcon.MOUSEEVENTF_LEFTDOWN,3000,1,0,0)
-wa.mouse_event(wcon.MOUSEEVENTF_LEFTUP,3000,1,0,0)
+wa.SetCursorPos((5000,1))
+wa.mouse_event(wcon.MOUSEEVENTF_LEFTDOWN,5000,1,0,0)
+wa.mouse_event(wcon.MOUSEEVENTF_LEFTUP,5000,1,0,0)
 
 wa.Sleep(100)
 
